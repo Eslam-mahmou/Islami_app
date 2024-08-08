f36284b29451ee46f90d6ca3bf0d3c964a3eb3a1
eslammahmoud <solom6112002@gmail.com>
2024-08-08 15:14:50 +0300

    Add splash view. Modify on provider in file main. Create home view.

--- lib/Moduls/SplashPage/SplashView.dart
+++ lib/Moduls/SplashPage/SplashView.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:islamy_app/Core/AppProvider/AppConfigProvider.dart';
+import 'package:islamy_app/Core/Utlis/AppAssets.dart';
+import 'package:islamy_app/layout.dart';
+import 'package:provider/provider.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
-static const String routeName = '/';
+
+  static const String routeName = '/';
 
   @override
   State<SplashView> createState() => _SplashViewState();
@@ -10,7 +17,22 @@ static const String routeName = '/';
 
 class _SplashViewState extends State<SplashView> {
   @override
+  void initState() {
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, HomeView.routeName);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var provide = Provider.of<AppConfigProvider>(context);
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: provide.isDarkMode()
+                  ? const AssetImage(ImageAssets.splah_dark_view)
+                  : const AssetImage(ImageAssets.splash_view),
+              fit: BoxFit.fill)),
+    );
   }
 }
